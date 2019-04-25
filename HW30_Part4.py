c0ed95d8e9537b83cada85058260d1c902969a24
Cecelia Paciaroni <cpatch>
2019-04-25 09:16:13 -0600

    Homework 30: Part 4

--- HW30_Part4.py
+++ HW30_Part4.py
@@ -7,7 +7,7 @@ from time import sleep
 spi=spidev.SpiDev()
 spi.open(0,0)
 
-def analaogInput(channel):
+def analogInput(channel):
     spi.max_speed_hz=1350000
     adc=spi.xfer2([1,(8+channel)<<4,0])
     data=((adc[1]&3)<<8)+adc[2]
